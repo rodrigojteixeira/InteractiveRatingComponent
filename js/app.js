d1fca02e2a18731c300bd45d9e878ca5aeab2455
Rodrigo Teixeira <rodrigojoseteixeira92@gmail.com>
2023-02-07 15:13:33 -0300

    Declaração das constantes

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
 const rating_cards = document.querySelectorAll(".ratings span");
 const submit = document.querySelector(".submit");
+const rate = document.getElementById("rate");
+const section = document.querySelector(".section");
+const thanks = document.querySelector(".thanks");
+
