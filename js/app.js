97bf127e0dba718e3773fc45cf47aaa5fb89e6bd
Rodrigo Teixeira <rodrigojoseteixeira92@gmail.com>
2023-02-10 15:56:38 -0300

    Seleção da nota

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 const rating_cards = document.querySelectorAll(".ratings span");
 const submit = document.querySelector(".submit");
-const rate = document.getElementById("rate");
+const rate_point = document.getElementById("rate");
 const section = document.querySelector(".section");
 const thanks = document.querySelector(".thanks");
 
+let rate = null;
+
+rating_cards.forEach((rating_card) => {
+    rating_card.addEventListener("click", (e) => {
+        const active = document.querySelector('.checked');
+        if (active) {
+            active.classList.remove("checked")
+        }
+        const card = e.target;
+        card.classList.add("checked");
+        rate = e.target.innerText;
+    })
+})
